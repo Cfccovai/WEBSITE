5987247f7f5c40fa2622da3159229c29dd177f3e
SimonRubrik <simon.ayyamperumal@rubrik.com>
2025-12-19 17:13:31 +0530

    1st upload

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,49 @@
+﻿using CFCCoimbatore.BAL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFCCoimbatore.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public ActionResult ShortHsitory()
+        {
+            
+            return View();
+        }
+
+        public ActionResult WhatWeBelieve()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Leadership()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult OurPurposeandAffliation()
+        {
+            
+            return View();
+        }
+        public ActionResult OurFinancialPolicy()
+        {
+            
+            return View();
+        }
+        public ActionResult OurMeetings()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using CFCCoimbatore.BAL;
+using CFCCoimbatore.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFCCoimbatore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ReadFilesAndProcessRecords readFile = new ReadFilesAndProcessRecords();
+        public ActionResult Index()
+        {
+
+            
+            var meetings = readFile.ReadMeetingDetails().Where(a => a.MeetingDate >=  DateTime.Now.AddHours(-3)).ToList();
+
+            if (meetings.Any() && meetings.Count() > 0)
+            {
+                ViewBag.Template = 1;
+                ViewBag.NextMeetingDate = meetings.OrderBy(a => a.MeetingDate).FirstOrDefault().MeetingDateString;
+                ViewBag.MeetingId = meetings.OrderBy(a => a.MeetingDate).FirstOrDefault().MeetingLink;
+                ViewBag.MeetingName = meetings.OrderBy(a => a.MeetingDate).FirstOrDefault().MeetingName;
+            }
+
+            return View(meetings);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Template = 1;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Template = 1;
+
+            
+            return View(readFile.ReadContact());
+        }
+
+        public ActionResult MaintenancePage()
+        {
+            ViewBag.Template = 0;
+            return View();
+        }
+
+        public ActionResult AboutZacPoonen()
+        {
+            ViewBag.Template = 1;
+            return View();
+        }
+    }
+}
